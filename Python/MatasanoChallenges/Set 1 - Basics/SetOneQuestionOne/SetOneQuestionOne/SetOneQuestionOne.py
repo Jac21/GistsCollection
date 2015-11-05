8aa1dadf0538faa839dcb929c487075277387bc8
Jac21 <jcantu521@gmail.com>
2015-11-05 10:15:56 -0600

    Re-did Python Matasano challenges, added question one

---
+++ Python/MatasanoChallenges/Set 1 - Basics/SetOneQuestionOne/SetOneQuestionOne/SetOneQuestionOne.py
@@ -0,0 +1,17 @@
+"""
+Convert hex to base64
+The string:
+
+49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+Should produce:
+
+SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+So go ahead and make that happen. You'll need to use this code for the rest of the exercises.
+"""
+
+#imports --
+import binascii, base64
+
+original_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+print base64.b64encode(original_string.decode('hex'))
