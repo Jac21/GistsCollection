1efef214cb74914a7be99197aa75187f720ec338
Jac21 <jcantu521@gmail.com>
2016-03-11 15:47:03 -0600

    Added Boilerplates folder, gulp-express-live reload boilerplate

---
+++ Boilerplates/GulpExpressBrowserSync/gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+
+
+gulp.task('default', ['browser-sync'], function () {
+});
+
+gulp.task('browser-sync', ['nodemon'], function() {
+	browserSync.init(null, {
+		proxy: "http://localhost:8080",
+        files: ["public/**/*.*"],
+        browser: "google chrome",
+        port: 7000,
+	});
+});
+gulp.task('nodemon', function (cb) {
+	
+	var started = false;
+	
+	return nodemon({
+		script: 'app.js'
+	}).on('start', function () {
+		if (!started) {
+			cb();
+			started = true; 
+		} 
+	});
+});
---
+++ Boilerplates/GulpExpressBrowserSync/public/test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var headerEl = document.getElementById('header');
+
+headerEl.addEventListener('click', function(e) {
+	headerEl.textContent = "Clicked!";
+	console.log(e);
+}, false);
