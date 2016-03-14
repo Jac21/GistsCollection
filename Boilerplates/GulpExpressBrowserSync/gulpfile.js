cd9755ed0a5363ec91ca8f664b2c101099d6b88c
Jac21 <jcantu521@gmail.com>
2016-03-14 12:48:54 -0500

    [Boilerplates] Adding linting to GulpExpressBrowserSync tasks

--- Boilerplates/GulpExpressBrowserSync/gulpfile.js
+++ Boilerplates/GulpExpressBrowserSync/gulpfile.js
@@ -1,21 +1,32 @@
 'use strict';
 
 var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var nodemon = require('gulp-nodemon');
+var browserSync = require('browser-sync');	// synch browser and code changes
+var nodemon = require('gulp-nodemon');			
+var lint = require('gulp-eslint');					// lint JS files
 
+// configuration object
+var config = {
+	proxyUrl: "http://localhost:8080",
+	browser: "google chrome",
+	port: 7000,
+	paths: {
+		files: "public/**/*.*",
+		js: "public/scripts/*.js"
+	}
+}
 
-gulp.task('default', ['browser-sync'], function () {
-});
-
+// browser-sync task
 gulp.task('browser-sync', ['nodemon'], function() {
 	browserSync.init(null, {
-		proxy: "http://localhost:8080",
-        files: ["public/**/*.*"],
-        browser: "google chrome",
-        port: 7000,
+		proxy: config.proxyUrl,
+        files: [config.paths.files],
+        browser: config.browser,
+        port: config.port,
 	});
 });
+
+// nodemon helper task
 gulp.task('nodemon', function (cb) {
 	
 	var started = false;
@@ -29,3 +40,18 @@ gulp.task('nodemon', function (cb) {
 		} 
 	});
 });
+
+// linting task
+gulp.task('lint', function() {
+	return gulp.src(config.paths.js)
+		.pipe(lint({config: 'eslint.config.json'}))
+		.pipe(lint.format());
+});
+
+// watch task for any html/js changes
+gulp.task('watch', function() {
+	gulp.watch(config.paths.js, ['lint']);
+});
+
+// default gulp tasks
+gulp.task('default', ['browser-sync', 'watch'], function () {});
--- Boilerplates/GulpExpressBrowserSync/public/scripts/test.js
+++ Boilerplates/GulpExpressBrowserSync/public/scripts/test.js
@@ -1,8 +1,9 @@
 'use strict';
 
+// header element var
 var headerEl = document.getElementById('header');
 
 headerEl.addEventListener('click', function(e) {
-	headerEl.textContent = "Clicked!";
+	headerEl.textContent = "Clicked!!";
 	console.log(e);
 }, false);
