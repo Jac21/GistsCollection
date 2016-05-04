c052d865abfd673bdb47e3aa31205e5129bb6b86
Jac21 <jcantu521@gmail.com>
2016-05-04 15:12:41 -0500

    [Riot] Added base Riot and Redux sample project

---
+++ Riot/RiotAndRedux/src/index.js
@@ -0,0 +1,19 @@
+var riot = require('riot');
+var redux = require('redux');
+require('./tags/sample-output.tag');
+
+var reducer = function(state = {title:'Default title'}, action) {
+	console.log(action);
+	switch(action.type) {
+		case 'CHANGE_TITLE':
+			// return state with new title...
+			return Object.assign({}, state, {title:action.data});
+		default:
+			// return current state
+			return state;
+	}
+}
+
+var reduxStore = redux.createStore(reducer);
+
+document.addEventListener('DOMContentLoaded', () => riot.mount('sample-output', {store:reduxStore}));
---
+++ Riot/RiotAndRedux/src/tags/sample-output.tag
@@ -0,0 +1,14 @@
+<sample-output>
+	<h1>{this.opts.store.getState().title}</h1>
+	
+	<form onsubmit={changeTitle}>
+		<input type="text" name="newTitle">
+		<input type="submit" value="change title">
+	</form>
+
+	<script>
+		changeTitle() {
+			this.opts.store.dispatch({type:"CHANGE_TITLE", data:this.newTitle.value});
+		};
+	</script>
+</sample-output>
