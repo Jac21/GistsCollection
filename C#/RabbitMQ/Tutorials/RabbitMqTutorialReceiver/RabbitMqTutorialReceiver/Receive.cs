d8ece30302ec2b74763a1e4b5a5a3165d1a6fbac
Jac21 <jcantu521@gmail.com>
2016-06-14 11:18:22 -0500

    Created RabbitMQ subfolder in C# folder, RabbitMQ Tutorial sender and receiver project

---
+++ C#/RabbitMQ/Tutorials/RabbitMqTutorialReceiver/RabbitMqTutorialReceiver/Receive.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMqTutorialReceiver
+{
+    class Receive
+    {
+        static void Main()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // Declare queue here as well, start receiver before the sender
+                    // to make sure queue exists before trying to consume messages
+                    channel.QueueDeclare(queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    // Push messages from the queue asynchronously
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("[X] Received {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: "hello",
+                        noAck: true,
+                        consumer: consumer);
+
+                    Console.WriteLine("Press [Enter] to exit");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ C#/RabbitMQ/Tutorials/RabbitMqTutorialSender/RabbitMqTutorialSender/Send.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMqTutorialSender
+{
+    /// <summary>
+    /// Message Sender
+    /// </summary>
+    class Send
+    {
+        static void Main()
+        {
+            // Create a connection to the server, broker on the local machine
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                // Declare a channel and queues for publishing
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = "Hello, World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    // Publish message to queue
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+
+                    Console.WriteLine("[X] Sent {0} with {1} byte size", message,
+                        Encoding.UTF8.GetByteCount(message));
+                }
+
+                Console.WriteLine("Press [Enter] to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
