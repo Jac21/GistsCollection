e6fd252db01d0eeb0e5d03383d2b7368793d62aa
Jac21 <jcantu521@gmail.com>
2016-08-30 11:52:56 -0500

    [C#] Added Genre and Gig models to GigHub project, migrated and updated DB

--- C#/GigHub/GigHub/Models/ApplicationUser.cs
+++ C#/GigHub/GigHub/Models/ApplicationUser.cs
@@ -1,5 +1,4 @@
-﻿using System.Data.Entity;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -15,19 +14,6 @@ namespace GigHub.Models
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
-        }
-    }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
         }
     }
 }
---
+++ C#/GigHub/GigHub/Models/Gig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+        public ApplicationUser Artist { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Venue { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
