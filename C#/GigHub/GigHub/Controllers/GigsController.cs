af18855bac4fd490f3427666b0e83bee03ff1ee6
Jac21 <jcantu521@gmail.com>
2016-08-31 15:50:10 -0500

    [C#] Created 'Add a Gig' view and view model in GigHub project

---
+++ C#/GigHub/GigHub/Controllers/GigsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using GigHub.Models;
+using GigHub.ViewModels;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ C#/GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GigHub.Models;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; } 
+    }
+}
