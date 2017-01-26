42a74e1677784fc5fde94f51723d51a36bba074e
Jac21 <jcantu521@gmail.com>
2017-01-25 21:03:40 -0600

    [Angular2] Made my first component in GettingStarted project, yay

--- Angular2/GetingStarted/app/app.component.ts
+++ Angular2/GetingStarted/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
+    <div><h1>{{pageTitle}}</h1>
+        <div>My First Component!</div>
+    </div>
     `
 })
-export class AppComponent { }
+export class AppComponent {
+    pageTitle: string = 'Acme Product Management';
+}
