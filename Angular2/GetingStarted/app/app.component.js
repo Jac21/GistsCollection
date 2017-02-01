dd73eb5a20e354719674f35108f84235688529ff
Jeremy Cantu <jcantu521@gmail.com>
2017-02-01 16:26:59 -0600

    [Angular2] Added DI to product-list

--- Angular2/GetingStarted/app/app.component.js
+++ Angular2/GetingStarted/app/app.component.js
@@ -6,6 +6,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
+var product_service_1 = require("./products/product.service");
 var AppComponent = (function () {
     function AppComponent() {
         this.pageTitle = 'Acme Product Management';
@@ -15,7 +16,8 @@ var AppComponent = (function () {
 AppComponent = __decorate([
     core_1.Component({
         selector: 'pm-app',
-        template: "\n    <div><h1>{{pageTitle}}</h1>\n        <pm-products></pm-products>\n    </div>\n    "
+        template: "\n    <div><h1>{{pageTitle}}</h1>\n        <pm-products></pm-products>\n    </div>\n    ",
+        providers: [product_service_1.ProductService]
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- Angular2/GetingStarted/app/products/product-list.component.ts
+++ Angular2/GetingStarted/app/products/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -13,46 +15,17 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = true;
-    listFilter: string = 'cart';
-    products: IProduct[] = [
-            {
-            'productId': 1,
-            'productName': 'Leaf Rake',
-            'productCode': 'GDN-0011',
-            'releaseDate': 'March 19, 2016',
-            'description': 'Leaf rake with 48-inch wooden handle.',
-            'price': 19.95,
-            'starRating': 3.2,
-            'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
-        },
-        {
-            'productId': 2,
-            'productName': 'Garden Cart',
-            'productCode': 'GDN-0023',
-            'releaseDate': 'March 18, 2016',
-            'description': '15 gallon capacity rolling garden cart',
-            'price': 32.99,
-            'starRating': 4.2,
-            'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
-        },
-        {
-            'productId': 5,
-            'productName': 'Hammer',
-            'productCode': 'TBX-0048',
-            'releaseDate': 'May 21, 2016',
-            'description': 'Curved claw steel hammer',
-            'price': 8.9,
-            'starRating': 4.8,
-            'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png'
-        }
-    ];
+    listFilter: string;
+    products: IProduct[];
+
+    constructor(private _productService: ProductService) { }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
     ngOnInit(): void {
-        console.log('In OnInit');
+        this.products = this._productService.getProducts();
     }
 
     onRatingClicked(message: string): void {
