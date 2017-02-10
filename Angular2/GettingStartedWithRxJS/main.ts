7bb7103cbc456360c11ddaf25c5b6849d2d8f1f6
Jeremy Cantu <jcantu521@gmail.com>
2017-02-10 16:44:26 -0600

    [Angular2] Updated RxJS starter project to work with data processing and promises, uninstalled es6 shim from typings and installed fetch and streams

--- Angular2/GettingStartedWithRxJS/main.ts
+++ Angular2/GettingStartedWithRxJS/main.ts
@@ -1,49 +1,58 @@
-import {Observable, Observer} from "rxjs";
+import {Observable} from "rxjs";
 
-//// To cut down on client code size...
-// import {Observable} from "rxjs/Observable";
-// import "rxjs/add/operator/map";
-// import "rxjs/add/operator/filter";
+let output = document.getElementById("output");
+let button = document.getElementById("button");
 
-let numbers = [1, 5, 10];
-let source = Observable.create(observer => {
-	let index = 0;
+let click = Observable.fromEvent(button, "click");
 
-	let produceValue = () => {
-		observer.next(numbers[index++]);
+function load(url: string) {
+	return Observable.create(observer => {
+		let xhr = new XMLHttpRequest();
 
-		if(index < numbers.length) {
-			setTimeout(produceValue, 2000);
-		} else {
-			observer.complete();
-		}
-	}
+		xhr.addEventListener("load", () => {
+			if(xhr.status === 200) {
+				let data = JSON.parse(xhr.responseText);
+				observer.next(data);
+				observer.complete()
+			} else {
+				observer.error(xhr.statusText);
+			}
+		});
 
-	produceValue();
-}).map(n => n * 2)
-	.filter(n => n > 4);
+		xhr.open("GET", url);
+		xhr.send();
+	}).retryWhen(retryStrategy({attempts: 3, delay: 1500}));
+}
 
-// Easier way of subscribing to an observer,
-// just pass in anon functions as next/error/complete
-source.subscribe(
-	value => console.log(`value: ${value}`),
-	e => console.log(`error: ${e}`),
-	() => console.log("complete")
-);
+function loadWithFetch(url: string) {
+	return Observable.defer(() => {
+		return Observable.fromPromise(fetch(url).then(r => r.json()));
+	});
+}
 
-// formal way of subscribing to an observer
-class MyObserver implements Observer<number>{
-	next(value) {
-		console.log(`value: ${value}`);
-	}
-
-	error(e) {
-		console.log(`error: ${e}`);
-	}
-
-	complete() {
-		console.log("complete");
+function retryStrategy({attempts = 4, delay = 1000}) {
+	return function(errors) {
+		return errors
+			.scan((acc, value) => {
+				console.log(acc, value);
+				return acc + 1;
+			}, 0)
+			.takeWhile(acc => acc < attempts)
+			.delay(delay);
 	}
 }
 
-// source.subscribe(MyObserver());
+function renderMovies(movies) {
+	movies.forEach(m => {
+		let div = document.createElement("div");
+		div.innerText = m.title;
+		output.appendChild(div);
+	});
+}
+
+click.flatMap(e => loadWithFetch("movies.json"))
+	.subscribe(
+		renderMovies,
+		e => console.log(`error: ${e}`),
+		() => console.log("complete")
+	);
