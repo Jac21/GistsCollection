3fa4e6759395eb8e9f7a3e067b8ac0f6e48c792d
Jeremy Cantu <jcantu521@gmail.com>
2017-02-13 15:17:39 -0600

    [Angular2] Implemented basic error handling/retry/unsubscribe logic in RxJS project

--- Angular2/GettingStartedWithRxJS/main.ts
+++ Angular2/GettingStartedWithRxJS/main.ts
@@ -1,46 +1,28 @@
 import {Observable} from "rxjs";
+import {load, loadWithFetch} from "./loader";
+
+/*
+let source = Observable.merge(
+	Observable.of(1),
+	Observable.from([2,3,4]),
+	Observable.throw(new Error("Stop!")),
+	Observable.of(5)
+).catch(e => {
+	console.log(`caught ${e}`);
+	return Observable.of(10);
+});
+
+source.subscribe(
+	value => console.log(`value: ${value}`),
+	error => console.log(`error: ${error}`),
+	() => console.log("complete")
+);
+*/
 
 let output = document.getElementById("output");
 let button = document.getElementById("button");
 
 let click = Observable.fromEvent(button, "click");
-
-function load(url: string) {
-	return Observable.create(observer => {
-		let xhr = new XMLHttpRequest();
-
-		xhr.addEventListener("load", () => {
-			if(xhr.status === 200) {
-				let data = JSON.parse(xhr.responseText);
-				observer.next(data);
-				observer.complete()
-			} else {
-				observer.error(xhr.statusText);
-			}
-		});
-
-		xhr.open("GET", url);
-		xhr.send();
-	}).retryWhen(retryStrategy({attempts: 3, delay: 1500}));
-}
-
-function loadWithFetch(url: string) {
-	return Observable.defer(() => {
-		return Observable.fromPromise(fetch(url).then(r => r.json()));
-	});
-}
-
-function retryStrategy({attempts = 4, delay = 1000}) {
-	return function(errors) {
-		return errors
-			.scan((acc, value) => {
-				console.log(acc, value);
-				return acc + 1;
-			}, 0)
-			.takeWhile(acc => acc < attempts)
-			.delay(delay);
-	}
-}
 
 function renderMovies(movies) {
 	movies.forEach(m => {
@@ -49,6 +31,12 @@ function renderMovies(movies) {
 		output.appendChild(div);
 	});
 }
+
+let subscription = 
+	load("movies.json")
+		.subscribe(renderMovies,
+			e => console.log(`error: ${e}`),
+			() => console.log("complete!"));
 
 click.flatMap(e => loadWithFetch("movies.json"))
 	.subscribe(
