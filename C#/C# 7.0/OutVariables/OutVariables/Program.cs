56f414a7e29af9ea0eb1f090422b26afc35727e3
Jeremy Cantu <jcantu521@gmail.com>
2017-04-17 16:37:29 -0500

    Created C# 7.0 sub-folder, example projects

---
+++ C#/C# 7.0/OutVariables/OutVariables/Program.cs
@@ -0,0 +1,42 @@
+﻿using static System.Console;    // static using statement, no need to rewrite "Console" below
+
+namespace OutVariables
+{
+    class Program
+    {
+        static void Main()
+        {
+            Runner runner = new Runner();
+            runner.RunTwo();
+        }
+    }
+
+    public class Runner
+    {
+        public void Run()
+        {
+            int hour;
+            int minutes;
+            int seconds;
+
+            GetTime(out hour, out minutes, out seconds);
+            WriteLine($"{hour}:{minutes}:{seconds}");
+        }
+
+        /// <summary>
+        /// No need to declare out variables in C# 7.0
+        /// </summary>
+        public void RunTwo()
+        {
+            GetTime(out int hour, out int minutes, out int seconds);
+            WriteLine($"{hour}:{minutes}:{seconds}");
+        }
+
+        public void GetTime(out int hour, out int minutes, out int seconds)
+        {
+            hour = 1;
+            minutes = 30;
+            seconds = 20;
+        }
+    }
+}
---
+++ C#/C# 7.0/Patterns/Patterns/Program.cs
@@ -0,0 +1,51 @@
+﻿using static System.Console;
+
+namespace Patterns
+{
+    class Program
+    {
+        static void Main()
+        {
+            Runner runner = new Runner();
+            runner.Run();
+        }
+    }
+
+    public class Runner
+    {
+        public void Run()
+        {
+            PrintSum(10);
+            PrintSumTwo("10");
+        }
+
+        public void PrintSum(object o)
+        {
+            if (o is null) return; // constant pattern
+
+            if(!(o is int i)) return; // type pattern (int)
+
+            int sum = 0;
+            for (int j = 0; j <= i; j++)
+            {
+                sum += j;
+            }
+
+            WriteLine($"The sum of 1 to {i} is {sum}");
+        }
+
+        public void PrintSumTwo(object o)
+        {
+            if (o is int i || o is string s && int.TryParse(s, out i))
+            {
+                int sum = 0;
+                for (int j = 0; j <= i; j++)
+                {
+                    sum += j;
+                }
+
+                WriteLine($"The sum of 1 to {i} is {sum}");
+            }
+        }
+    }
+}
