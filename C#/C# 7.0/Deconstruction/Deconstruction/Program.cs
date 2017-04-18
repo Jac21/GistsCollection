6bb4031ee7575857161e8aabce80fc4882e334e8
Jeremy Cantu <jcantu521@gmail.com>
2017-04-18 16:49:02 -0500

    Added tuple example projects to C# 7.0 sub-folder

---
+++ C#/C# 7.0/Deconstruction/Deconstruction/Program.cs
@@ -0,0 +1,40 @@
+﻿using static System.Console;
+
+namespace Deconstruction
+{
+    class Program
+    {
+        static void Main()
+        {
+            Runner runner = new Runner();
+            runner.Run();
+        }
+    }
+
+    internal class Runner
+    {
+        public void Run()
+        {
+           (int hour, int minutes, int seconds) = GetTime();
+
+            if (minutes > 30)
+            {
+                hour++;
+            }
+
+            WriteLine($"{hour}:{minutes}:{seconds}");
+        }
+
+        public (int hour, int minutes, int seconds) GetTime()
+        {
+            return (1, 35, 40);
+        }
+
+        public void RunTwo()
+        {
+            int hour, minutes, seconds;
+            (hour, minutes, seconds) = GetTime();
+            WriteLine($"The time using local variables: {hour}:{minutes}:{seconds}");
+        }
+    }
+}
