ed32fe6438d6d84db654c6d48e788e9cf1d3b6b4
Jeremy Cantu <jcantu521@gmail.com>
2017-10-31 14:40:57 -0500

    Added Functional sub-folder in C# parent folder, exercises from Manning book

---
+++ C#/Functional/FunctionalProgrammingManning/FunctionalProgrammingManning/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace FunctionalProgrammingManning
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Testing
+            WriteLine(ChapterOne.SwapDivide(2, 4));
+
+            foreach (var number in ChapterOne.GetMod(1, 20, 3))
+            {
+                WriteLine(number);
+            }
+
+            ReadLine();
+        }
+    }
+}
