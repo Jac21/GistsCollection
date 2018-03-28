151eef8cb1c879062ce71d05154cc784dcc047bb
Jeremy Cantu <jcantu521@gmail.com>
2018-03-28 09:48:15 -0500

    Cleanup

--- Node.js/Modules/Gulp/GulpExpressBrowserSync/public/scripts/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// header element var
-var headerEl = document.getElementById('header');
-
-headerEl.addEventListener('click', function(e) {
-	headerEl.textContent = "Clicked!!";
-	console.log(e);
-}, false);
--- React/WebpackBabelProjectSkeleton/index.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-//https://medium.com/@thisbejim/zero-baloney-literal-step-by-step-for-creating-a-babel-6-react-and-hot-reloading-webpack-project-90562eafe0e0#.6qad0s6lr
-
-ReactDOM.render(
-	<h1>Hello, React!</h1>,
-	document.getElementById('example')
-);
