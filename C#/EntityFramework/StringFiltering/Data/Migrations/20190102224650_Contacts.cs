7ec1104711af1283dcc568b378ffdb14bf4de654
Jeremy Cantu <jcantu521@gmail.com>
2019-01-03 09:21:37 -0600

    Added .NET Core EntityFramework string filtering example project

---
+++ C#/EntityFramework/StringFiltering/Data/Migrations/20190102224650_Contacts.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StringFiltering.Data.Migrations
+{
+    public partial class Contacts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true),
+                    Zip = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "Id", "Address", "City", "FirstName", "LastName", "State", "Zip" },
+                values: new object[] { 1, "123 Main Street", "Nashville", "Bob", "Smith", "TN", "35970" });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "Id", "Address", "City", "FirstName", "LastName", "State", "Zip" },
+                values: new object[] { 2, "1 Sun Lane", "Knoxville", "Sam", "Smith", "TN", "48909" });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "Id", "Address", "City", "FirstName", "LastName", "State", "Zip" },
+                values: new object[] { 3, "750 10th Street", "Chattanooga", "Clark", "Swift", "TN", "91590" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
---
+++ C#/EntityFramework/StringFiltering/Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StringFiltering.Data;
+
+namespace StringFiltering.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var contacts = await context.Contacts.ToListAsync();
+
+            // Like Queries
+            // Allows usages of wildcards that were not possible using string function translation 
+            // that was previously the only option.
+            var likeQuery = await context.Contacts
+                .Where(c => EF.Functions.Like(c.LastName, "S_it%"))
+                .ToListAsync();
+
+            // Greater Than/Less Than
+            // It is important to not try and use any of the overloads of String.Compare, 
+            // or you will end up with client-side evaluation of your query. 
+            var stringCompareQuery = await context.Contacts
+                // ReSharper disable once StringCompareIsCultureSpecific.1
+                .Where(c => String.Compare(c.FirstName, "D") > 0)
+                .ToListAsync();
+
+            // SELECT[c].[Id], [c].[Address], [c].[City], [c].[FirstName], [c].[LastName], [c].[State], [c].[Zip]
+            // FROM[Contacts] AS[c]
+            // WHERE[c].[FirstName] > N'D'
+
+            return Page();
+        }
+    }
+}
