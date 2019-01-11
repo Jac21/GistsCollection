a9770f431d23c6555c0b5f9e602cb7491c31afa8
Jeremy Cantu <jcantu521@gmail.com>
2019-01-11 15:00:02 -0600

    Added SOLID principles mini-reference-project under C# reference folder

---
+++ C#/Reference/Solid/Solid/Helpers/Database.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solid.Helpers
+{
+    public class Database
+    {
+        public void Add(string postMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogError(string anErrorOccured, string toString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddAsTag(string postMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotifyUser(string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OverrideExistingMention(string user, string postMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetUnhandledPostsMessages()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C#/Reference/Solid/Solid/Principles/I.cs
@@ -0,0 +1,40 @@
+﻿namespace Solid.Principles
+{
+    /// <summary>
+    /// I — Interface segregation principle
+    /// No client should be forced to depend on methods it does not use.
+    /// </summary>
+    public class I
+    {
+        // Let’s look at an example of how to violate the interface segregation principle.
+
+        public interface IPost
+        {
+            void CreatePost();
+        }
+
+        public interface IPostNew
+        {
+            void CreatePost();
+            void ReadPost();
+        }
+
+        // In this example, let’s pretend that I first have an IPost interface 
+        // with the signature of a CreatePost() method. 
+        // Later on, I modify this interface by adding a new method ReadPost(), 
+        // so it becomes like the IPostNew interface.
+
+        // This is where we violate the interface segregation principle.
+        // Instead, simply create a new interface.
+
+        public interface IPostCreate
+        {
+            void CreatePost();
+        }
+
+        public interface IPostRead
+        {
+            void ReadPost();
+        }
+    }
+}
