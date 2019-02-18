6415abbb501fc6d0b1b91df24db5c2fdffe89b24
Jeremy Cantu <jcantu521@gmail.com>
2019-02-18 16:25:50 -0600

    Added Extension Methods project under C# folder

---
+++ C#/Experiments/ExtensionMethods/CreativeExtensionMethods/CreativeExtensionMethods/Enums/FileFormatExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CreativeExtensionMethods.Enums
+{
+    public enum FileFormat
+    {
+        PlainText,
+        OfficeWord,
+        Markdown
+    }
+
+    /// <summary>
+    /// Adding methods to enums
+    /// </summary>
+    public static class FileFormatExtensions
+    {
+        public static string GetFileExtensions(this FileFormat fileFormat)
+        {
+            if (fileFormat == FileFormat.PlainText)
+                return "txt";
+
+            if (fileFormat == FileFormat.OfficeWord)
+                return "docx";
+
+            if (fileFormat == FileFormat.Markdown)
+                return "md";
+
+            // thrown when we add a new file format but forget to add
+            // corresponding file extension
+            throw new ArgumentNullException(nameof(fileFormat));
+        }
+    }
+}
---
+++ C#/Experiments/ExtensionMethods/CreativeExtensionMethods/CreativeExtensionMethods/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CreativeExtensionMethods.Enums;
+using CreativeExtensionMethods.Interfaces;
+using CreativeExtensionMethods.Models;
+
+namespace CreativeExtensionMethods
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("--------- Enums ----------");
+
+            var format = FileFormat.Markdown;
+            var fileExt = format.GetFileExtensions(); // "md"
+            Console.WriteLine($"output.{fileExt}");
+
+            Console.WriteLine("--------- Models ----------");
+
+            var closedCaptionTrack = new ClosedCaptionTrack("en-US", new List<ClosedCaption>
+            {
+                new ClosedCaption("First Caption", TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1)),
+                new ClosedCaption("Second Caption", TimeSpan.FromMinutes(2), TimeSpan.FromMinutes(2)),
+                new ClosedCaption("Third Caption", TimeSpan.FromMinutes(3), TimeSpan.FromMinutes(3))
+            });
+
+            Console.WriteLine(closedCaptionTrack.GetByTime(TimeSpan.FromMinutes(1)).Text);
+
+            Console.WriteLine("--------- Interfaces ----------");
+
+            var exportService = new ExportService();
+
+            var model = new Model
+            {
+                Name = "Model One"
+            };
+
+            // exportService.SaveToFile(model, Directory.GetCurrentDirectory());
+            exportService.SaveToMemeory(model);
+
+            Console.ReadLine();
+        }
+    }
+}
