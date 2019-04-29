e7c3179f4790a28a5af09c9df99e8aa242a6ae1a
Jeremy Cantu <mail@jeremycantu.com>
2019-04-28 21:12:46 -0500

    Added AsyncAwaitCancellation project in C# folder

---
+++ C#/Reference/Multithreading&Parallelism/AsyncAwaitCancellation/AsyncAwaitCancellation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitCancellation
+{
+    public class Program
+    {
+        private static async Task Task_NetworkBound()
+        {
+            await new HttpClient().GetStringAsync("https://www.jeremycantu.com");
+        }
+
+        private static async Task<string> Task_NetworkBound_T()
+        {
+            return await new HttpClient().GetStringAsync("https://www.github.com/Jac21");
+        }
+
+        /// <summary>
+        /// Usage
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine(await Task_NetworkBound_T().CancelAfter(3000));
+
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(3000);
+
+            Console.WriteLine(await Task_NetworkBound_T().CancelAfter(cts.Token));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Reference/Multithreading&Parallelism/AsyncAwaitCancellation/AsyncAwaitCancellation/TaskCancellationExtension.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitCancellation
+{
+    public static class TaskCancellationExtension
+    {
+        /// <summary>
+        /// Add cancellation functionality to Task<typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="task"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<T> CancelAfter<T>(this Task<T> task, CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            using (cancellationToken.Register(
+                s => ((TaskCompletionSource<bool>)s).TrySetResult(true), tcs))
+            {
+                if (task != await Task.WhenAny(task, tcs.Task))
+                {
+                    throw new OperationCanceledException(cancellationToken);
+                }
+            }
+
+            return await task;
+        }
+
+        /// <summary>
+        /// Add cancellation functionality to Tasks
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="task"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task CancelAfter(this Task task, CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            using (cancellationToken.Register(
+                s => ((TaskCompletionSource<bool>)s).TrySetResult(true), tcs))
+            {
+                if (task != await Task.WhenAny(task, tcs.Task))
+                {
+                    throw new OperationCanceledException(cancellationToken);
+                }
+            }
+
+            await task;
+        }
+
+        /// <summary>
+        /// Add cancellation functionality to Task<typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="task"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<T> CancelAfter<T>(this Task<T> task, int milliseconds)
+        {
+            var cts = new CancellationTokenSource();
+
+            cts.CancelAfter(milliseconds);
+
+            var tcs = new TaskCompletionSource<bool>();
+
+            using (cts.Token.Register(
+                s => ((TaskCompletionSource<bool>)s).TrySetResult(true), tcs))
+            {
+                if (task != await Task.WhenAny(task, tcs.Task))
+                {
+                    throw new OperationCanceledException(cts.Token);
+                }
+            }
+
+            return await task;
+        }
+
+        /// <summary>
+        /// Add cancellation functionality to Tasks
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="task"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task CancelAfter(this Task task, int milliseconds)
+        {
+            var cts = new CancellationTokenSource();
+
+            cts.CancelAfter(milliseconds);
+
+            var tcs = new TaskCompletionSource<bool>();
+
+            using (cts.Token.Register(
+                s => ((TaskCompletionSource<bool>)s).TrySetResult(true), tcs))
+            {
+                if (task != await Task.WhenAny(task, tcs.Task))
+                {
+                    throw new OperationCanceledException(cts.Token);
+                }
+            }
+
+            await task;
+        }
+    }
+}
