5d6e458266c62201ffcbae9c6a334a66a133174e
Jeremy Cantu <jcantu521@gmail.com>
2022-04-03 12:43:18 -0500

    Fleshed out basic kernal with bootimage lib

--- Rust/OS/my_os/src/main.rs
+++ Rust/OS/my_os/src/main.rs
@@ -3,8 +3,18 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello, my OS!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
     loop {}
